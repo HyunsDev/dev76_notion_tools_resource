950da711782ad4e572a7a8a36faa39f212754b43
hyuns <hyuns@hyuns.me>
2020-10-08 23:04:09 +0900

    update

--- header/script.js
+++ header/script.js
@@ -1,11 +1,10 @@
 var content = document.getElementById('header');
-var parent = content.parentNode;
 document.body.insertBefore(content, document.body.firstChild);
 
-$(document).ready(function () {
-    $(".listed").hide();
-    $(".dropdowndtn").click(function () {
-        $(".listed").toggle();
-    })
-});
+$(function() {
+    $(".listed_mob").hide();
+})
 
+$(".dropdowndtn").click(function() {
+    $(".listed_mob").toggle();
+})
